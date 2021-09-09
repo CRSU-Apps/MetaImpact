54619bc1f4e25869da6509c59f4d6c6a7469b010
Clareece Nevill <clareece_@hotmail.com>
2021-09-09 15:14:42 +0100

    Created base UI with user inputs for evidence synthesis

--- server.R
+++ server.R
@@ -11,4 +11,30 @@ library(shiny)
 # Server Content #
 #----------------#
 function(input, output, session) {
+  
+# Summary sentence of meta-analysis #  
+output$SynthesisSummary <- renderText({
+  if (input$FreqBaye=='frequentist') {
+  if (input$ContBin=='binary') {
+    paste("Below are the results from a ", strong(input$FixRand), "-effects meta-analysis of ", strong(input$OutcomeBina), "s using ", strong("frequentist"), " methodology. 
+    To change the model options, please see the synthesis options above.")
+  }
+  else if (input$ContBin=='continuous') {
+    paste("Below are the results from a ", strong(input$FixRand), "-effects meta-analysis of ", strong(input$OutcomeCont), "s using ", strong("frequentist"), " methodology. 
+    To change the model options, please see the synthesis options above.")
+  }
+  }
+  else if (input$FreqBaye=='Bayesian') {
+    if (input$ContBin=='binary') {
+      paste("Below are the results from a ", strong(input$FixRand), "-effects meta-analysis of ", strong(input$OutcomeBina), "s using ", strong("Bayesian"), " methodology with vague prior ", strong(input$prior), ". 
+    To change the model options, please see the synthesis options above.")
+    }
+    else if (input$ContBin=='continuous') {
+      paste("Below are the results from a ", strong(input$FixRand), "-effects meta-analysis of ", strong(input$OutcomeCont), "s using ", strong("Bayesian"), " methodology with vague prior ", strong(input$prior), ". 
+    To change the model options, please see the synthesis options above.")
+    }
+  }
+})
+  
+  
 }
--- ui.R
+++ ui.R
@@ -4,10 +4,55 @@
 # load libraries #
 #----------------#
 library(shiny)
+library(shinythemes)
+library(shinyBS)
 
 # load user-written functions #
 #-----------------------------#
 
 # UI Content #
 #------------#
-shinyUI()
+shinyUI(navbarPage(title="MetaImpact",
+                   theme = shinytheme("readable"),
+                   
+# Home Tab #
+#----------#
+
+tabPanel("Home"),
+                   
+# Data Tab #
+#----------#
+                   
+tabPanel("Data",
+         radioButtons("ContBin", "Continuous or Binary", c("Continuous" = "continuous", "Binary" = "binary"))),
+                   
+# Evidence Synthesis Tab #
+#------------------------#
+                   
+tabPanel("Evidence Synthesis",
+         # Inputs #
+         fluidRow(bsCollapse(bsCollapsePanel(title="Synthesis Options", style='primary',
+                  column(3, conditionalPanel(condition = "input.ContBin=='continuous'",
+                                   radioButtons("OutcomeCont", "Outcome for continuous data:", c("Mean Difference (MD)" = "MD","Standardised Mean Difference (SMD)" = "SMD"))),
+                            conditionalPanel(condition = "input.ContBin=='binary'",
+                                   radioButtons("OutcomeBina", "Outcome for binary data:", c("Odds Ratio (OR)" = "OR","Risk Ratio (RR)" = "RR", "Risk Difference (RD)" = "RD")))),
+                  column(3, radioButtons("FixRand", "Model selection:", c("Fixed-effects model (FE)" = "fixed", "Random-effects model (RE)" = "random"))),
+                  column(3, radioButtons("FreqBaye", "Methodology choice:", c("Frequestist" = "frequentist", "Bayesian" = "Bayesian"))),
+                  column(3, conditionalPanel(condition = "input.FreqBaye=='Bayesian'",
+                                   radioButtons("prior", "Choice of vague prior:", c("Option 1" = "1", "Option 2" = "2"))))))),
+         # Outputs #
+         fluidRow(align = 'center',
+           htmlOutput("SynthesisSummary"),
+           column(4), #Network plot
+                  column(6,offset=2) #Forest plot
+                  )),
+
+# Sample Size Calculator Tab #
+#----------------------------#
+
+tabPanel("Sample Size Calculator"),
+
+# Education Tab #
+#---------------#
+
+tabPanel("Education")))
