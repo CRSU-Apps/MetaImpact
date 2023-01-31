de9993da3c3d64287b50db1072c324832155e595
Clareece Nevill <clareece_@hotmail.com>
2023-01-31 08:31:46 +0000

    Optimised Bayesian functions (RStan)

--- StanFunctions.R
+++ StanFunctions.R
@@ -18,12 +18,17 @@ GenBinaryRandomFit = stan("GenBinaryRandom.stan", data = test_data_stage1, chain
 
 BinaryRandomNewStudy = stan(fit=GenBinaryRandomFit,
                             data = test_data_stage1,
-                            chains = 1,
-                            iter = 500,
-                            warmup = 400,
-                            thin = 1,
+                            chains = 2,
+                            iter = 1000,                    # The number of samples here should equal the number of samples I want my power estimate to be based off
+                            warmup = 500,
+                            thin = 10,                      # Don't want lots of simulations for new study, but want a non-correlated sample
                             init = list(chain1=list(d = 0, deltaNew = 0, sigma=0.1, mu = c(0, 0,0,0,0,0),
-                                                    delta = c(0, 0,0,0,0,0))))
+                                                    delta = c(0, 0,0,0,0,0)),
+                                        chain2=list(d = 1, deltaNew = 1, sigma=0.8, mu = c(1, 1,1,1,1,1),
+                                                    delta = c(1,1,1,1,1,1))))
+print(BinaryRandomNewStudy)  # check diagnostics
+traceplot(BinaryRandomNewStudy, pars="sigma", include=TRUE)
+# 0.255 seconds
 
 ## Extract values of rtNew and rcNew from each 'simulation'
 rtNew = as.data.frame(BinaryRandomNewStudy)$rtNew
@@ -45,11 +50,13 @@ SimBinaryRandomFit = stan("SimBinaryRandom.stan", data = test_data_stage2, chain
 BinaryRandomResults = stan(fit=SimBinaryRandomFit,
                            data = test_data_stage2,
                            chains=1,
-                           iter=500,
-                           warmup=400,
+                           iter=1200,                          # here the iterations just need to be enough to have proper convergence
+                           #warmup=400,
                            thin=1,
-                           init = list(chain1=list(d = rep(0,100), sigma=rep(0.1,100), mu = matrix(rep(c(0,0,0,0,0,0,0), 100), nrow=100, byrow=TRUE),
-                                                   delta = matrix(rep(c(0,0,0,0,0,0,0), 100), nrow=100, byrow=TRUE))))
+                           init = list(chain1=list(d = rep(-0.29,100), sigma=rep(0.39,100), mu = matrix(rep(c(-0.7,-0.76,0.72,-2.99,-0.29,-2.46,0), 100), nrow=100, byrow=TRUE),
+                                                   delta = matrix(rep(c(-0.63,-0.31,-0.09,-0.22,-0.33,-0.06,-0.35), 100), nrow=100, byrow=TRUE))))    # added initial values from stage 1
+#457.05 seconds
+print(BinaryRandomResults, pars="sigma", include=TRUE) # checking sigma Rhat <1.1
 
 # Extract ORs #
 
@@ -59,4 +66,10 @@ ORSummary = as.data.frame(summary(BinaryRandomResults, pars="OR", include=TRUE)$
 # Calculate power #
 ORSummary$Significant <- (ORSummary$`2.5%`<1 & ORSummary$`97.5%`<1) | (ORSummary$`2.5%`>1 & ORSummary$`97.5%`>1)
 Power <- sum(as.numeric(ORSummary$Significant))/length(ORSummary$Significant)
-# Result was 37%, quite different to 7% with WinBUGS, but could be a wide CI
+# Compare results
+prop.test(sum(as.numeric(ORSummary$Significant)),length(ORSummary$Significant))  # 2% (95% CI: 0.3%, 7.7%)
+prop.test(3,100) # WinBUGS 3% (95% CI: 0.8%, 9.2%)    happy with that!
+# Crowther's Stata functions give: 32.% (95% CI: 23.02%, 42.08%) -> not so happy :(
+# Frequentist on app gives: 0% power (95% CI: 0% to 4.6%) -> which is better, but what does this say about how my frequentist is so different to Michael's?
+
+# See if Alex's WinBUGS code gives same answer as figure 6 in Crowther's paper.
