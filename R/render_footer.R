551de0ff389d3f3cb686c1ce57f753e769ef8827
JanionNevill <janion.nevill@leicester.ac.uk>
2024-03-12 13:55:01 +0000

    Updated funding statement header

--- R/render_footer.R
+++ R/render_footer.R
@@ -1,42 +1,33 @@
 renderFooter <- function() {
-  tagList(
-    fluidRow(
-      shinydashboard::box(
-        tags$a(
-          href = "https://www.nihr.ac.uk/",
-          target = "_blank",
-          img(
-            src = "images/funded-by-nihr-logo.png",
-            alt = "Funded by NIHR",
-            class = "footer-img nihr-img img-responsive center-block"
-          )
-        ),
-        includeMarkdown("md/funding_statement.md"),
-        width = 12
-      )
+  div(
+    p(
+      "THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT",
+      "NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.",
+      "IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,",
+      "WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE",
+      "OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
     ),
-    fluidRow(
-      column(
-        width = 6,
-        tags$a(
-          href = "https://www.gla.ac.uk/",
-          target = "_blank",
-          img(
-            src = "images/university-of-glasgow-logo.png", 
-            alt = "University of Glasgow", 
-            class = "footer-img img-responsive center-block"
-          )
-        )
+    wellPanel(
+      div(
+        style = "display: inline;",
+        img(src = 'funded-by-nihr-logo.png', width = "55%")
       ),
-      column(
-        width = 6,
-        tags$a(
-          href = "https://le.ac.uk/", target = "_blank",
-          img(
-            src = "images/university-of-leicester-logo.jpg",
-            alt = "University of Leicester",
-            class = "footer-img img-responsive center-block"
-          )
+      div(
+        style = "display: inline;",
+        img(src = 'CRSU_logo.png', width = "40%")
+      ),
+      div(
+        tags$strong("Funding and Support Acknowledgement:"),
+        tags$p(
+          "MetaPairwise is part of the Complex Reviews Synthesis Unit (CRSU) suite of evidence synthesis apps.",
+          "The development of these apps is currently funded (majority) and overseen by the Evidence Synthesis Group @ CRSU (NIHR153934).",
+          "Further details of other funders and support, current and past, can be found ",
+          tags$a("on our GitHub page", href = "https://github.com/CRSU-Apps/.github/wiki/Detailed-Funding-Statement", target = "_blank"),
+          ". The views expressed are those of the author(s) and not necessarily those of the NIHR or the Department of Health and Social Care."
+        ),
+        tags$p(
+          "More information about the UK NIHR Complex Reviews Synthesis Unit (CRSU) can be found ",
+          tags$a("on our website.", href = "https://www.gla.ac.uk/research/az/evidencesynthesis/apps-materials-guidence/", target = "_blank"),
         )
       )
     )
